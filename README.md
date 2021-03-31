d6a8f53a194818c6a0399ab2af0d28b6379ced5a
Spencer Owen <owenspencer@gmail.com>
2021-03-30 22:23:16 -0600

    jr brown

--- README.md
+++ README.md
@@ -1,24 +1,11 @@
+# Convert mp4 to gif
 
 
-```bash
-input_file='jrbrownform.mp4'
+### Broken videos
 
-start_time=07
-stop_time=15
-output_file='jrBrownFrom-Part1.gif'
-```
-```bash
-ffmpeg -ss ${start_time} \
--to ${stop_time} \
--i ${input_file} \
--filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" \
-${output_file}
-```
+- Jr. Blue Form
 
-07-13
-13-23
-23-32
-32-46
-46-51
 
- 
+### Missing videos
+
+- 
--- gif.sh
+++ gif.sh
@@ -2,13 +2,14 @@
 # Usage:
 # source juniorbrown.env
 # ./gif.sh
+set -u
+source $1
 
-source juniorbrown.env
-
-start_time=${START_TIME}
 part=1
-for stop_time in "${SPLITS[@]}"
+for i in "${SPLITS[@]}"
 do
+  start_time=$(echo $i | cut -d'^' -f1)
+  stop_time=$(echo $i | cut -d'^' -f2)
   echo "start: ${start_time}; end ${stop_time}"
   ffmpeg -ss ${start_time} -to ${stop_time} -i "${INPUT_FILE}" -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y ${OUTPUT_FILE}-part${part}.gif
   start_time=${stop_time}
