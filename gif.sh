236c41fcfef1a7cde7d4a1a525ae7c6cf4e60095
Spencer Owen <owenspencer@gmail.com>
2021-04-04 23:11:32 -0600

    add download into script

--- gif.sh
+++ gif.sh
@@ -5,6 +5,20 @@
 set -u
 source $1
 
+function download_video {
+  if [ ! -f "input/${INPUT_FILE}" ]; then
+    echo "input/${INPUT_FILE} does not exist, downloading ${VIDEO_URL}"
+    filename=$(youtube-dl ${VIDEO_URL} --get-filename)
+    filename=$(echo ${filename/webm/mp4})
+    youtube-dl -f 135 ${VIDEO_URL}
+    mv "${filename}" input/${INPUT_FILE}
+  else
+    echo "input/${INPUT_FILE} already exists"
+  fi
+}
+
+download_video
+
 part=1
 for i in "${SPLITS[@]}"
 do
@@ -17,7 +31,7 @@ do
   else
     output=${OUTPUT_FILE}-complete.gif
   fi
-  ffmpeg -ss ${start_time} -to ${stop_time} -i "${INPUT_FILE}" -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y output/${output}
+  ffmpeg -ss ${start_time} -to ${stop_time} -i "input/${INPUT_FILE}" -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y output/${output}
   start_time=${stop_time}
   part=$((part+1))
 done
