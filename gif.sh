1ff31f1af5bc7d403b8c9596487daa034da11025
Spencer Owen <owenspencer@gmail.com>
2021-05-11 23:17:07 -0600

    higher res

--- gif.sh
+++ gif.sh
@@ -5,12 +5,14 @@
 set -u
 source $1
 
+REZ=${RESOLUTION:-480}
+
 function download_video {
   if [ ! -f "input/${INPUT_FILE}" ]; then
     echo "input/${INPUT_FILE} does not exist, downloading ${VIDEO_URL}"
     filename=$(youtube-dl ${VIDEO_URL} --get-filename)
     filename=$(echo ${filename/webm/mp4})
-    youtube-dl -f 135 ${VIDEO_URL}
+    youtube-dl -f 22 ${VIDEO_URL}
     mv "${filename}" input/${INPUT_FILE}
   else
     echo "input/${INPUT_FILE} already exists"
@@ -27,11 +29,11 @@ do
 
   echo "start: ${start_time}; end ${stop_time}"
   if [ "${#SPLITS[@]}" -gt 1 ]; then
-    output=${OUTPUT_FILE}-part${part}.gif
+    output=${OUTPUT_FILE}-part${part}-${REZ}.gif
   else
-    output=${OUTPUT_FILE}-complete.gif
+    output=${OUTPUT_FILE}-complete-${REZ}.gif
   fi
-  ffmpeg -ss ${start_time} -to ${stop_time} -i "input/${INPUT_FILE}" -filter_complex "[0:v] fps=12,scale=480:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y output/${output}
+  ffmpeg -ss ${start_time} -to ${stop_time} -i "input/${INPUT_FILE}" -filter_complex "[0:v] fps=12,scale=${REZ}:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" -y output/${output}
   start_time=${stop_time}
   part=$((part+1))
 done
